e644db618ca5ddd6abd4f1b442efbb2f39454f6e
sayymeer <sameer.yadv8@gmail.com>
2024-03-24 18:08:47 +0530

    commit implementes

--- pkg/git/commit.go
+++ pkg/git/commit.go
@@ -1,6 +1,8 @@
 package git
 
 import (
+	"bytes"
+	"compress/zlib"
 	"crypto/sha1"
 	"fmt"
 	"io"
@@ -13,13 +15,22 @@ import (
 func CommitHandler() error {
 	files, _ := utils.Ls()
 	wd, _ := os.Getwd()
+	objectDir := path.Join(wd, ".sasy", "objects")
 	for _, file := range files {
 		filePath := path.Join(wd, file)
 		byteContent, _ := os.ReadFile(filePath)
-		content := fmt.Sprintf("%s %d/0%s", "blob", len(byteContent), string(byteContent))
+		content := fmt.Sprintf("%s %d\x00%s", "blob", len(byteContent), string(byteContent))
 		h := sha1.New()
 		io.WriteString(h, content)
-		fmt.Printf("%x\n", h.Sum(nil))
+		name := fmt.Sprintf("%x", h.Sum(nil))
+		dirName := name[0:2]
+		fileName := name[2:]
+		os.Mkdir(path.Join(objectDir, dirName), os.ModePerm)
+		var b bytes.Buffer
+		w := zlib.NewWriter(&b)
+		w.Write([]byte(content))
+		w.Close()
+		os.WriteFile(path.Join(objectDir, dirName, fileName), b.Bytes(), 0644)
 	}
 	return nil
 }
